c335c530f74f0f529f30a6276b89d1db21939221
rocky <rb@dustyfeet.com>
2021-01-23 18:24:07 -0500

    More exports

--- mathics_scanner/__init__.py
+++ mathics_scanner/__init__.py
@@ -6,6 +6,8 @@ Wolfram-language scanner
 from mathics_scanner.version import __version__
 
 from mathics_scanner.characters import (
+    aliased_characters,
+    named_characters,
     replace_unicode_with_wl,
     replace_wl_with_plain_text,
 )
