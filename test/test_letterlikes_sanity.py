979b7fb58addbcfca4ac3f44ac85f0056195b283
Pablo Emilio Escobar Gaviria <pablo-escobar@riseup.net>
2021-01-31 16:40:07 +0000

    Added sanity checks for letter-like character

---
+++ test/test_letterlikes_sanity.py
@@ -0,0 +1,21 @@
+from mathics_scanner.generate.build_tables import DEFAULT_DATA_DIR
+import yaml
+import json
+
+def check_letterlikes_sanity(yaml_data: dict, json_data: dict):
+    letterlikes = json_data["letterlikes"]
+
+    yaml_llc = list(yaml_data.values()).count(lambda v: v["is-letter-like"])
+    json_llc = len(letterlikes)
+
+    assert (
+        yaml_llc == json_llc
+    ), f"the YAML table has {yaml_llc} letter-like characters but the JSON files lists {json_llc} letterlike characters"
+
+
+def test_letterlikes_sanity():
+    with open(DEFAULT_DATA_DIR / "named-characters.yml", "r") as yaml_file, open(DEFAULT_DATA_DIR / "characters.json", "r") as json_file:
+        yaml_data = yaml.load(yaml_file, Loader=yaml.FullLoader)
+        json_data = yaml.load(json_file)
+        check_letterlikes_sanity(yaml_data, json_data)
+
