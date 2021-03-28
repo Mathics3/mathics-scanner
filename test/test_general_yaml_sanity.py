a0aad4f11895366657adee51b27b91200c14ba16
rocky <rb@dustyfeet.com>
2021-03-28 15:03:40 -0400

    Adding operator testing

--- test/test_general_yaml_sanity.py
+++ test/test_general_yaml_sanity.py
@@ -5,6 +5,7 @@ import unicodedata
 
 yaml_data = load_mathics_character_yaml()
 
+
 def check_attr_is_invertible(attr: str):
     for v in yaml_data.values():
         if attr in v:
@@ -37,6 +38,43 @@ def test_yaml_field_names():
             "wl-unicode-name",
         }
         assert diff == set(), f"Item {k} has unknown fields {diff}"
+
+
+def test_operators():
+    ascii_seen = set()
+    operator_name_seen = set()
+    dup_operators = set(["Apply"])
+    for k, v in yaml_data.items():
+        if "ascii" in v:
+            if len(v["ascii"]) > 1:
+                assert (
+                    "operator-name" in v
+                ), "In %s: ASCII with more than one characters must be an operator" % (
+                    k
+                )
+                pass
+        else:
+            assert "wl-unicode" in v, (
+                "In %s: there must be either an ascii name or have a wl-unicode"
+                % k
+            )
+        if "operator-name" not in v:
+            continue
+
+        assert not v["is-letter-like"], "Operator %s should not be letter-like" % k
+
+        assert "ascii" in v, 'Operator %s should have an "ascii" field' % k
+        ascii = v["ascii"]
+        assert ascii not in ascii_seen
+        ascii_seen.add(ascii)
+
+        operator_name = v["operator-name"]
+        if operator_name in dup_operators:
+            continue
+        assert (
+            operator_name not in operator_name_seen
+        ), "Operator name %s has operator %s already been seen" % (operator_name, k)
+        operator_name_seen.add(operator_name)
 
 
 def test_wl_unicode_name():
@@ -77,7 +115,9 @@ def test_unicode_name():
             try:
                 expected_name = " + ".join(unicodedata.name(c) for c in uni)
             except ValueError:
-                import pdb; pdb.set_trace()
+                import pdb
+
+                pdb.set_trace()
                 raise ValueError(
                     f"{k}'s unicode-equivalent doesn't have a unicode name (it's not valid unicode)"
                 )
@@ -97,6 +137,7 @@ def test_unicode_name():
                 "unicode-equivalent-name" not in v
             ), f"{k} has unicode-equivalent-name set to {v['unicode-equivalent-name']} but it doesn't have a unicode equivalent"
 
+
 def test_wl_unicode():
     for k, v in yaml_data.items():
         if "operator-name" in v:
@@ -105,6 +146,7 @@ def test_wl_unicode():
                 # have a wl-unicode equivalent.
                 continue
         assert "wl-unicode" in v, f"{k} has no wl-unicode attribute"
+
 
 def test_general_yaml_sanity():
     # Check if required attributes are in place
