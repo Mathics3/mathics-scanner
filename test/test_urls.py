9ca255007eb0f267225612e50fc474e3bb5adca9
rocky <rb@dustyfeet.com>
2024-09-16 16:45:11 -0400

    Add WL reference URLs...
    and add a (slow) test program to retrieve and check these.

---
+++ test/test_urls.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import os
+from urllib.error import HTTPError, URLError
+from urllib.request import urlopen
+
+import pytest
+
+from mathics_scanner.load import load_mathics_character_yaml
+
+yaml_data = load_mathics_character_yaml()
+
+
+# This test is slow, so do only on request!
+@pytest.mark.skipif(
+    not os.environ.get("MATHICS_LINT"), reason="Lint checking done only when specified"
+)
+def test_yaml_urls():
+    for k, v in yaml_data.items():
+        # This code can be used to start some point down the list
+        # if k < "UndirectedEdge":
+        #     continue
+        url = v.get("wl-reference")
+        if url:
+            try:
+                with urlopen(url) as response:
+                    html = response.read()
+                    assert html, f"should have been able to get HTML for {k} ({url})"
+                    print(f"got {k}")
+            except* (URLError, HTTPError):
+                assert False, f"{k} ({url}) fails"
