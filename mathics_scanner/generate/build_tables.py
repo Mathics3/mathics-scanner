9b2214e93ff695c7bde6d649feab63651fb147ca
R. Bernstein <rocky@users.noreply.github.com>
2024-11-27 15:22:51 -0500

    Another pass over operator data (#98)
    mathics_scanner/characters.py:
    * Clarify "associativity" field. Check validity of field in htestgs
    * Remove a"actual-precedence" field
    
    Change name:
    characters.json -> character_tables.json to match what it is in Mathics3
    
    Makefile: force mathics_scanner/data/operators.json build more often by
    including it as a dependency of "build".

--- mathics_scanner/generate/build_tables.py
+++ mathics_scanner/generate/build_tables.py
@@ -270,7 +270,7 @@ ALL_FIELDS = [
     "-o",
     show_default=True,
     type=click.Path(writable=True),
-    default=DEFAULT_DATA_DIR / "characters.json",
+    default=DEFAULT_DATA_DIR / "character-tables.json",
 )
 @click.argument(
     "data_dir", type=click.Path(readable=True), default=DEFAULT_DATA_DIR, required=False
